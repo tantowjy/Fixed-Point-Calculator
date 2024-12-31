4780fa8840fbec68fcb078361761b3911e18b8c6
Tanto Wijaya <tanto.twjy@gmail.com>
2024-12-31 22:13:54 +0700

    Update decimal_to_binary.v

--- src/decimal_to_binary.v
+++ src/decimal_to_binary.v
@@ -4,6 +4,8 @@ module decimal_to_binary (
     input [3:0] decimal_input_b,          // 1-digit decimal hundredths
     output [15:0] output_num
 );
+
+    // Registers
     reg [7:0] num_tenths;
     reg [7:0] num_hundredths;
     reg [15:0] num_decimal_mul;
@@ -16,9 +18,6 @@ module decimal_to_binary (
     always @(*) begin
         // Default value
         num_internal <= 16'b0;
-
-        // Konversi desimal ke bilangan pecahan 6-bit dengan pembulatan
-        // fractional_6bit = (decimal_input * 64) / 100;
         
         num_tenths <= {4'b0, decimal_input_a};
         num_hundredths <= {4'b0, decimal_input_b};
@@ -33,4 +32,5 @@ module decimal_to_binary (
         // Combine inputs into final number
         num_internal <= {6'b0, decimal_int, fractional_6bit};
     end
-endmodule
+
+endmodule 
